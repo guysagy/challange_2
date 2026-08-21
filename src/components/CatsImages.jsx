373e19ebf244405a6c46e6d166fdc5cb9eb715e8
harrypotter96 <ember.winston@hotmail.com>
2026-08-21 00:34:36 -0700

    wip - update 2

--- src/components/CatsImages.jsx
+++ src/components/CatsImages.jsx
@@ -2,11 +2,39 @@ import React , { useEffect, useState } from 'react';
 import useFetchCatsData from './useFetchCatsData';
 import './CatsImages.css';
 
+function CatImage(props) {
+    let {id, url, width, height} = props.data;
+    const maxSize = 200; // pixels
+    const widthRatio = maxSize/width;
+    const heightRatio = maxSize/height;
+    const ratio = Math.min(widthRatio, heightRatio);
+    return (
+        <div className='cat-image-container'>
+            <img src={url} width={width*ratio} height={height*ratio} alt=''/>
+        </div>
+    )
+}
 
-function CatsImages(props) {
+function CatsImages() {
+    const state = useFetchCatsData();
+    console.log('state = ', state);
+
+    let contents = "";
+    if (state.error) {
+        contents = state.error
+    } else if (state.isLoading) {
+        contents = 'Loading ...';
+    } else if (Array.isArray(state.data)) {
+        contents = state.data.map(item => <CatImage data={item} key={item.id}/>)
+    } else {
+        contents = 'Unexpected error; pleaes reload the page';
+    }
+
     return (
         <div className='cats-images-container'>
-            {props.children}
+            {
+                contents
+            }
         </div>
     )
 }
--- src/components/useFetchCatsData.js
+++ src/components/useFetchCatsData.js
@@ -1,5 +1,91 @@
-function useFetchCatsData() {
+import React, { useEffect, useState } from 'react';
 
+const CATS_IMAGES_URL = "https://api.thecatapi.com/v1/images/search?limit=10&page=1";
+const CAT_BREED_DATA = "https://api.thecatapi.com/v1/images"; // ${id}"
+
+function useFetchCatsData() {
+    const [state, setState] = useState({
+        isLoading: false, 
+        error: '', 
+        data: [],
+    });
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        async function doFetch() {
+
+            try {
+                setState((prev) => ({
+                    ...prev, 
+                    isLoading: true
+                }))
+
+                const response = await fetch(CATS_IMAGES_URL, {
+                    signal: controller.signal,
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch cats data; http error: ${response.status}`)
+                }
+
+                const data = await response.json();
+
+                const breedPromises = []
+
+                data.map(item => {
+                    const promise = new Promise((accept) => {
+                        let breedData = {};
+
+                        const url = `${CAT_BREED_DATA}/${item.id}`;
+                        // console.log('breed url  = ', url);
+                        fetch(url, {
+                            signal: controller.signal,
+                        }).then(response => {
+                            if (response.ok) {
+                                return response.json();
+                            }
+                        }).then((json) => {
+                            breedData = json.breeds[0];
+                            // console.log('breedData  = ', breedData);
+                        }).catch(error => {
+
+                        }).finally(() => {
+                            item.breed = breedData;
+                            accept();
+                        });
+                    });
+                    breedPromises.push(promise);
+                })
+
+                await Promise.allSettled(breedPromises);
+
+                setState((prev) => ({
+                    ...prev, 
+                    isLoading: false,
+                    data: data
+                }));
+
+            } catch(error) {
+                if (controller.signal.aborted) {
+                    return;
+                }
+                setState((prev) => ({
+                    ...prev, 
+                    isLoading: false,
+                    error: error
+                }));
+            } 
+        }
+
+        doFetch();
+
+        return () => {
+            controller.abort('Unmounting');
+        }
+    }, []);
+
+    return state;
 }
 
 export default useFetchCatsData;
