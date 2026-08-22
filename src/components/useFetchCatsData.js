958b613428008f276a8cedc3a57a186d22f79e26
harrypotter96 <ember.winston@hotmail.com>
2026-08-21 18:10:17 -0700

    working

--- src/components/useFetchCatsData.js
+++ src/components/useFetchCatsData.js
@@ -34,28 +34,24 @@ function useFetchCatsData() {
                 // const breedPromises = []
 
                 const breedPromises = data.map(item => {
-                    const promise = new Promise((accept) => {
-                        let breedData = {};
-
-                        const url = `${CAT_BREED_DATA}/${item.id}`;
-                        fetch(url, {
-                            signal: controller.signal,
-                        }).then(response => {
-                            if (response.ok) {
-                                return response.json();
-                            } else {
-                                throw new Error('Fetching breed data for ' + item.id + ' failed with http status ' + response.status);
-                            }
-                        }).then((json) => {
-                            breedData = json.breeds[0];
-                        }).catch(error => {
-                            console.error(error);
-                        }).finally(() => {
-                            item.breed = breedData;
-                            accept();
-                        });
+                    let breedData = {};
+                    const url = `${CAT_BREED_DATA}/${item.id}`;
+                    return fetch(url, {
+                        signal: controller.signal,
+                    }).then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } else {
+                            throw new Error('Fetching breed data for ' + item.id + ' failed with http status ' + response.status);
+                        }
+                    }).then((json) => {
+                        breedData = json.breeds[0];
+                    }).catch(error => {
+                        console.error(error);
+                    }).finally(() => {
+                        item.breed = breedData;
+                        accept();
                     });
-                    return promise;
                 })
 
                 await Promise.allSettled(breedPromises);
